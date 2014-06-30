8e3918db84d540fbff0c200cf87353d0992639ba
Leonardo Bravo <leonardo.bravo@mail.udp.cl>
2014-06-30 17:30:48 -0400

    App veersión beta - Lunes

--- APPS/Arduino_uno/assets/www/js/power.js
+++ APPS/Arduino_uno/assets/www/js/power.js
@@ -1,12 +1,12 @@
 function on(){
     var Status = "1"
-    url = "http://192.168.1.254:5000/on"
+    url = "http://192.168.1.116:5000/on"
     data =  {status:Status}
     
     	$.ajax({
             url: url,
             // dataType: 'json',
-            type : "post",
+            type : "get",
             data:data,
             crossDomain: true,
             complete: function(xhr, statusText){
@@ -24,13 +24,13 @@ function on(){
 
 function off(){
     var Status = "0"
-    url = "http://192.168.1.254:5000/off"
+    url = "http://192.168.2.116:5000/off"
     data =  {status:Status}
     
     	$.ajax({
             url: url,
             // dataType: 'json',
-            type : "post",
+            type : "get",
             data:data,
             crossDomain: true,
             complete: function(xhr, statusText){
